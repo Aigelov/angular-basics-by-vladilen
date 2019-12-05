7f06162ce91cbee971889a2c0ffc264fbe213ad2
Aigelov <aigelov@gmail.com>
2019-12-05 23:50:14 +0600

    ContentChild

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, ContentChild, ElementRef, Input, OnInit} from '@angular/core';
 import {Post} from '../app.component';
 
 @Component({
@@ -7,10 +7,15 @@ import {Post} from '../app.component';
   styleUrls: ['./post.component.scss']
 })
 export class PostComponent implements OnInit {
+
   @Input() post: Post;
+
+  @ContentChild('info', {static: true}) infoRef: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
+    console.log(this.infoRef.nativeElement);
   }
 
 }
