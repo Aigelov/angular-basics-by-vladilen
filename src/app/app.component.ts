df6de5d24187d6087652c4a0086e0dd03bd0e201
Aigelov <aigelov@gmail.com>
2019-12-05 18:30:41 +0600

    Post, PostForm components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  id?: number;
+  title: string;
+  text: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-basics-by-vladilen';
+  posts: Post[] = [
+    {id: 1, title: 'I want to learn Angular', text: `I'm still learning components`},
+    {id: 2, title: 'Next block', text: `Will be about directives and pipes`},
+  ];
+
+  updatePosts(post: Post) {
+    this.posts.unshift(post);
+    console.log('Post', this.posts);
+  }
 }
