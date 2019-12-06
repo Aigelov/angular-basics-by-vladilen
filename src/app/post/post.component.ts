7ff3c64086ae65e3479ae64d019b3113aeb64f31
Aigelov <aigelov@gmail.com>
2019-12-06 11:38:50 +0600

    Life cycle hooks, change detection strategy, encapsulation of css

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,21 +1,69 @@
-import {Component, ContentChild, ElementRef, Input, OnInit} from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit, AfterViewChecked, AfterViewInit, ChangeDetectionStrategy, Component, ContentChild, DoCheck,
+  ElementRef, EventEmitter, Input,
+  OnChanges,
+  OnDestroy,
+  OnInit, Output,
+  SimpleChanges, ViewEncapsulation
+} from '@angular/core';
 import {Post} from '../app.component';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+  styleUrls: ['./post.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
 })
-export class PostComponent implements OnInit {
+export class PostComponent implements
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy {
 
   @Input() post: Post;
+  @Output() removePostByID = new EventEmitter<number>();
 
   @ContentChild('info', {static: true}) infoRef: ElementRef;
 
-  constructor() { }
-
-  ngOnInit() {
-    console.log(this.infoRef.nativeElement);
+  removePost() {
+    this.removePostByID.emit(this.post.id);
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges');
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
 }
