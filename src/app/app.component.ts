bb6d7a7bde6bf4cf661f9ae5dcfeaa3fcf5be1f6
Aigelov <aigelov@gmail.com>
2019-12-07 20:30:06 +0600

    Clear project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {Subscription, Subject} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,24 +6,4 @@ import {Subscription, Subject} from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
-  subscription: Subscription;
-  stream$: Subject<number> = new Subject<number>();
-  counter = 0;
-
-  constructor() {
-    this.subscription = this.stream$
-      .subscribe(value => {
-        console.log('Subscribe', value);
-      });
-  }
-
-  next() {
-    this.counter++;
-    this.stream$.next(this.counter);
-  }
-
-  stopInterval() {
-    this.subscription.unsubscribe();
-  }
 }
