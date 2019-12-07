d4ee2e01a56cf06788b0853e50e2f00dafb3b54e
Aigelov <aigelov@gmail.com>
2019-12-07 15:56:48 +0600

    Clear project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
-
-export interface Post {
-  title: string;
-  text: string;
-}
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  promise: Promise<string> = new Promise<string>(resolve => {
-    setTimeout(() => {
-      resolve('Promise Resolved');
-    }, 2000);
-  });
-
-  date$: Observable<Date> = new Observable(observer => {
-    setInterval(() => {
-      observer.next(new Date());
-    }, 1000);
-  });
-  dateInfo: Date;
-
-  ngOnInit() {
-    this.date$.subscribe(date => {
-      this.dateInfo = date;
-    });
-  }
+export class AppComponent {
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import {FilterPipe} from './pipes/filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FilterPipe
+    AppComponent
   ],
   imports: [
     BrowserModule,
