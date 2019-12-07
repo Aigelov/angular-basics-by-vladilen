6db6a8ead8a370ba940abc08316f90feaefb7c79
Aigelov <aigelov@gmail.com>
2019-12-07 20:28:30 +0600

    RxJS bonus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Subscription, Subject} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,24 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  subscription: Subscription;
+  stream$: Subject<number> = new Subject<number>();
+  counter = 0;
+
+  constructor() {
+    this.subscription = this.stream$
+      .subscribe(value => {
+        console.log('Subscribe', value);
+      });
+  }
+
+  next() {
+    this.counter++;
+    this.stream$.next(this.counter);
+  }
+
+  stopInterval() {
+    this.subscription.unsubscribe();
+  }
 }
