7a4c4be832335f92239315314eeea56624b5c0ed
Aigelov <aigelov@gmail.com>
2019-12-08 16:44:41 +0600

    Clear project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { SwitchComponent } from './switch/switch.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SwitchComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
