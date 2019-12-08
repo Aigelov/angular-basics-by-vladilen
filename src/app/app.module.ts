ae0b3c792e3b2dbd9cd596d4815080f59c2d062f
Aigelov <aigelov@gmail.com>
2019-12-08 16:28:40 +0600

    NgModel, NgModelChange, VALUE_ACCESSOR, Provider, ControlValueAccessor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { SwitchComponent } from './switch/switch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SwitchComponent
   ],
   imports: [
     BrowserModule,
