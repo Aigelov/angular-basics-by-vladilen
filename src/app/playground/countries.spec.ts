34d5899f9edcbec9612f3e692be06f682a4c53dd
Aigelov <aigelov@gmail.com>
2019-12-14 23:52:17 +0600

    Clear project

--- src/app/playground/countries.spec.ts
+++
@@ -1,8 +0,0 @@
-import {countries} from './countries';
-
-describe('countries', () => {
-  it('should contain countries codes', () => {
-    const result = countries();
-    expect(result).toEqual(['KZ', 'UA', 'BY']);
-  });
-});
--- src/app/playground/countries.ts
+++
@@ -1,3 +0,0 @@
-export function countries() {
-  return ['KZ', 'UA', 'BY'];
-}
